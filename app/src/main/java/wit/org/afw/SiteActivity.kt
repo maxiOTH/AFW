48f31b43074980e8a29d9d3971c17a12098a74ca
unknown <maximilian-bergbauer@web.de>
2019-01-02 12:28:31 +0100

    First commit

---
+++ app/src/main/java/wit/org/afw/SiteActivity.kt
@@ -0,0 +1,12 @@
+package wit.org.afw
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SiteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_site)
+    }
+}
